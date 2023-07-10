96cf4e9d8444eaf0c095878bf5b31f7c86ed1a7a
Amit Kapoor <amit.kapoor27@gmail.com>
2023-07-10 14:34:57 +0530

    Update Header.tsx

--- src/Components/Layouts/Header.tsx
+++ src/Components/Layouts/Header.tsx
@@ -9,8 +9,8 @@ interface HeaderProps {
     return (
       <AppBar position="fixed" sx={{ backgroundColor: '#fff' ,color:'black' }}>
         <Toolbar>
-          <Box sx={{ display: 'flex', alignItems: 'center' }}>
-            <img src="https://www.exlservice.com/themes/exl_service/exl_logo_rgb_orange_pos_94.png" alt="EXL" width="50" height="50" />
+          <Box sx={{ display: 'flex', align-items: 'centre' }}>
+            <img src="/path/to/your/logo.png" alt="Company Logo" width="50" height="50" />
             <Typography variant="h6" component="h1" sx={{ ml: 1 }}>
               EXL Service
             </Typography>
